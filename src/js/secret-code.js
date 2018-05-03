88c48a72fdb2cb6a443bf5e22c1563555136bbee
Christopher Silich <chrissilich@gmail.com>
2018-05-03 15:50:49 -0400

    .

--- src/js/secret-code.js
+++ src/js/secret-code.js
@@ -2,21 +2,7 @@ class SecretCode {
 
 	constructor(code, callback) {
 		
-		let entered = '';
-
-		document.body.addEventListener('keyup', function (event) {
-			console.log(event.key)
-			entered += event.key
-
-			console.log('code', code)
-			console.log('entered', entered)
-
-			if (entered.indexOf(code) !== -1) {
-				console.log('match!')
-				entered = ''
-				callback()
-			}
-		})
+		
 
 	}
 }
