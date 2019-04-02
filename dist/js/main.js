10cc0131fe2d9bd2071d276a42072e885ad8e771
Chris Silich <chris.silich@gmail.com>
2019-04-02 17:23:56 -0400

    ready for students

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,2 +1,10 @@
-"use strict";new SecretCode("abcd",function(){console.log("fireworks!"),document.body.style.background="red",document.body.style.color="white",document.body.style.fontSize="5em",document.body.innerText="secret code entered!"});
+"use strict";
+
+new SecretCode('abcd', function () {
+  console.log('fireworks!');
+  document.body.style.background = "red";
+  document.body.style.color = "white";
+  document.body.style.fontSize = "5em";
+  document.body.innerText = 'secret code entered!';
+});
 //# sourceMappingURL=main.js.map
--- dist/js/secret-code.js
+++ dist/js/secret-code.js
@@ -3,6 +3,6 @@
 function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
 
 var SecretCode = function SecretCode(secretCode, callback) {
-	_classCallCheck(this, SecretCode);
+  _classCallCheck(this, SecretCode);
 };
 //# sourceMappingURL=secret-code.js.map
